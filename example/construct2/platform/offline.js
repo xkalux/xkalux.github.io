fca25f9589f8b59860e7f8f9a47c6ed595df92eb
Ekkalak Chimjan <25895495+x-kalux@users.noreply.github.com>
2025-02-05 15:55:13 +0700

    restructure game 2d game example

---
+++ example/construct2/platform/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1738743763,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tilemap.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/enemy-sheet0.png",
+		"images/enemy-sheet1.png",
+		"images/apple-sheet0.png",
+		"images/apple-sheet1.png",
+		"images/scorespritefont.png",
+		"images/livesspritefont.png",
+		"media/collect-ring.ogg",
+		"media/game-die.ogg",
+		"media/game-over.ogg",
+		"media/jump-up.ogg",
+		"media/background-music.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
